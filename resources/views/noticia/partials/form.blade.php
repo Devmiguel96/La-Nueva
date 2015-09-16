f525edcc66598dbf15d11b289ef7c69e0d06a27b
Miguel Zambrano Herrera <zmiguel96@gmail.com>
2015-09-16 11:46:33 -0500

    form modificado

--- resources/views/noticia/partials/form.blade.php
+++ resources/views/noticia/partials/form.blade.php
@@ -2,13 +2,13 @@
 	{!! Form::label('titulo', 'Titulo') !!}
 	{!! Form::text('titulo',null,['class'=>'form-control'])!!}
 </div>
-	<div class="form-group">
-     	{!! Form::label('detalle', 'Detalle') !!}
-		{!! Form::text('detalle',null,['class'=>'form-control','id'=>'ckeditor'])!!}
-		<script>
-            CKEDITOR.replace( 'ckeditor' );
-        </script>
-	</div>
+<div class="form-group">
+     {!! Form::label('detalle', 'Detalle') !!}
+	{!! Form::text('detalle',null,['class'=>'form-control','id'=>'ckeditor'])!!}
+	<script>
+        CKEDITOR.replace( 'ckeditor' );
+    </script>
+</div>
 <div class="form-group">
 	{!! Form::label('categorias_id', 'Tipo de Noticia') !!}
 	<select name="categorias_id" id="categorias_id" class="form-control">
