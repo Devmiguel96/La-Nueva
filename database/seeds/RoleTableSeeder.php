68a7fc53c274b9f9e2a9aa96ffcfea3e38eab241
Miguel Zambrano Herrera <zmiguel96@gmail.com>
2015-09-17 14:32:12 -0500

    seeder

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -9,13 +9,11 @@ class RoleTableSeeder extends Seeder
 	public function run()
 	{	     
 	        \DB::table('roles')->insert(array(
-	        	'id'=>'1',
 				'nombre' =>'admin',
 				'created_at'=>date('Y-m-d H:m:s'),
 				'updated_at'=>date('Y-m-d H:m:s')
 		    ));
 		    \DB::table('roles')->insert(array(
-		    	'id'=>'2',
 				'nombre' =>'user',
 				'created_at'=>date('Y-m-d H:m:s'),
 				'updated_at'=>date('Y-m-d H:m:s')
