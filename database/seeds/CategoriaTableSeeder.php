0d526a7366c64331b8764e6b43188bc1c57d57c7
Miguel Zambrano Herrera <zmiguel96@gmail.com>
2015-09-17 14:29:02 -0500

    seddermogig

--- database/seeds/CategoriaTableSeeder.php
+++ database/seeds/CategoriaTableSeeder.php
@@ -38,6 +38,15 @@ class CategoriaTableSeeder extends Seeder
 				'created_at'=>date('Y-m-d H:m:s'),
 				'updated_at'=>date('Y-m-d H:m:s')
 		));
+		\DB::table('usuarios')->insert(array(
+				'nombre' =>'Admin',
+				'apellido'=>'Admin',
+				'email'=>'admin@hotmail.com',
+				'password'=>\Hash::make('administrador'),
+				'roles_id'=>'1',
+				'created_at'=>date('Y-m-d H:m:s'),
+				'updated_at'=>date('Y-m-d H:m:s')
+		));
 	}
 }
 
--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -9,24 +9,18 @@ class RoleTableSeeder extends Seeder
 	public function run()
 	{	     
 	        \DB::table('roles')->insert(array(
+	        	'id'=>'1',
 				'nombre' =>'admin',
 				'created_at'=>date('Y-m-d H:m:s'),
 				'updated_at'=>date('Y-m-d H:m:s')
 		    ));
 		    \DB::table('roles')->insert(array(
+		    	'id'=>'2',
 				'nombre' =>'user',
 				'created_at'=>date('Y-m-d H:m:s'),
 				'updated_at'=>date('Y-m-d H:m:s')
 		    ));
-		    \DB::table('usuarios')->insert(array(
-				'nombre' =>'Admin',
-				'apellido'=>'Admin',
-				'email'=>'admin@hotmail.com',
-				'password'=>\Hash::make('administrador'),
-				'roles_id'=>'1',
-				'created_at'=>date('Y-m-d H:m:s'),
-				'updated_at'=>date('Y-m-d H:m:s')
-		    ));
+		    
 	    
 		
 	}
